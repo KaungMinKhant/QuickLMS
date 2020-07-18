5d5edc0d8cb7f9b0a5142160d11957d6933325b7
Kaung Min Khant <phenomenalkaung.com>
2020-07-18 19:42:03 +0630

    teacher side finished

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -21,12 +21,20 @@
     <link rel="stylesheet" type="text/css" href="/styles/responsive.css">
     <link rel="stylesheet" type="text/css" href="/styles/course.css">
     <link rel="stylesheet" type="text/css" href="/styles/course_responsive.css">
+    <link rel="stylesheet" type="text/css" href="/styles/coursepage.css">
 
     <!-- Plugins -->
     <link href="/plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" type="text/css" href="/plugins/OwlCarousel2-2.2.1/owl.carousel.css">
     <link rel="stylesheet" type="text/css" href="/plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
     <link rel="stylesheet" type="text/css" href="/plugins/OwlCarousel2-2.2.1/animate.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
 
 
 
--- resources/views/lesson.blade.php
+++ resources/views/lesson.blade.php
@@ -1,53 +1,85 @@
 @extends('layouts.home')
 
-@section('sidebar')
-    <p class="lead">{{ $lesson->course->title }}</p>
 
-    <div class="list-group">
-        @foreach ($lesson->course->publishedLessons as $list_lesson)
-            <a href="{{ route('lessons.show', [$list_lesson->course_id, $list_lesson->slug]) }}" class="list-group-item"
-                @if ($list_lesson->id == $lesson->id) style="font-weight: bold" @endif>{{ $loop->iteration }}. {{ $list_lesson->title }}</a>
-        @endforeach
-    </div>
-@endsection
+
+
 
 @section('main')
+<div class="home">
+    <div class="breadcrumbs_container">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <ul class="breadcrumbs_list d-flex flex-row align-items-center justify-content-start">
+                        <li><a href="index.html">home</a></li>
+                        <li><a href="courses.html">course</a></li>
+                        <li><a href="{{ route('lessons.show', [$lesson->course_id, $lesson->slug]) }}">{{$lesson->title}}</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-    <h2>{{ $lesson->title }}</h2>
-
-    @if ($purchased_course || $lesson->free_lesson == 1)
-        {!! $lesson->full_text !!}
-
-        @if ($test_exists)
-            <hr />
-            <h3>Test: {{ $lesson->test->title }}</h3>
-            @if (!is_null($test_result))
-                <div class="alert alert-info">Your test score: {{ $test_result->test_result }}</div>
-            @else
-            <form action="{{ route('lessons.test', [$lesson->slug]) }}" method="post">
-                {{ csrf_field() }}
-                @foreach ($lesson->test->questions as $question)
-                    <b>{{ $loop->iteration }}. {{ $question->question }}</b>
-                    <br />
-                    @foreach ($question->options as $option)
-                        <input type="radio" name="questions[{{ $question->id }}]" value="{{ $option->id }}" /> {{ $option->option_text }}<br />
-                    @endforeach
-                    <br />
+<div class="container coursepage_container">
+    <div class="row coursepage_row">
+        <div>
+            <!-- Lesson Navigation -->
+            <ul class="nav nav-tabs" role="tablist">
+                @foreach ($lesson->course->publishedLessons as $list_lesson)
+                <li role="presentation"><a href="{{ route('lessons.show', [$list_lesson->course_id, $list_lesson->slug]) }}">{{ $list_lesson->title }}</a></li>
                 @endforeach
-                <input type="submit" value=" Submit results " />
-            </form>
-            @endif
-            <hr />
-        @endif
+            </ul>
+            <!-- End of Lesson Navigation -->
+            @if ($purchased_course || $lesson->free_lesson == 1)
+            <div class="tab-content">
+                <div role="tabpanel" class="tab-pane active" id="l1">
+                    <div class="container lessoncontent">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">{{$lesson->title}}</div>
+                            <div class="panel-body coursepanel">
+                                {!! $lesson->full_text !!}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Downloadable Files</h3>
+                        </div>
+                        <div class="panel-body">
+                            @foreach($lesson->getMedia('downloadable_files') as $media)
+                            <p class="form-group">
+                                <p>{{ $media->name }} ({{ $media->size }} KB)</p>
+                                <a href="{{ $media->getUrl() }}" target="_blank" class="btn btn-xs btn-success">Download</a>
+                                <input type="hidden" name="downloadable_files_id[]" value="{{ $media->id }}">
+                            </p>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     @else
-        Please <a href="{{ route('courses.show', [$lesson->course->slug]) }}">go back</a> and buy the course.
+    <div class="tab-content">
+        <div role="tabpanel" class="tab-pane active" id="l1">
+            <div class="container lessoncontent">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Please <a href="{{ route('courses.show', [$lesson->course->slug]) }}">go back</a> and buy the course.</div>
+                </div>
+            </div>
+        </div>
+    </div>
     @endif
 
-    @if ($previous_lesson)
-        <p><a href="{{ route('lessons.show', [$previous_lesson->course_id, $previous_lesson->slug]) }}"><< {{ $previous_lesson->title }}</a></p>
-    @endif
-    @if ($next_lesson)
-        <p><a href="{{ route('lessons.show', [$next_lesson->course_id, $next_lesson->slug]) }}">{{ $next_lesson->title }} >></a></p>
-    @endif
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
 
 @endsection
