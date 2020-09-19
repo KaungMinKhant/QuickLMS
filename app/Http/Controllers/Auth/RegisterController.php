3e5b36708aed214295221ef86b3aa01659b6a61c
Kaung Min Khant <phenomenalkaung.com>
2020-09-19 06:58:13 +0630

    pee b lah?

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,7 +65,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $request=$this->saveFiles($data['profile_pic']);
+        $request=$this->saveFiles($data);
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
